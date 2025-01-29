110d793475bc98789667d66d0f7061c361b9ff53
benank <gh@benank.com>
2025-01-28 14:16:11 -1000

    Update assets + animation

--- src/lib/client/components/CreatingPage.svelte
+++ src/lib/client/components/CreatingPage.svelte
@@ -2,8 +2,8 @@
 	<div class="flex flex-col items-center justify-center gap-2">
 		<h1 class="mb-4 text-balance text-center text-2xl font-semibold">Creating your workout...</h1>
 		<video
-			src="/assets/seven.mp4"
-			class="aspect-square w-60 rounded-lg"
+			src="/assets/seven.webm"
+			class="fadeIn aspect-square w-60"
 			controls={false}
 			muted
 			autoplay
@@ -11,3 +11,22 @@
 		></video>
 	</div>
 </div>
+
+<style>
+	.fadeIn {
+		animation: fadeIn ease-in 0.5s;
+		animation-fill-mode: forwards;
+		opacity: 0;
+	}
+
+	@keyframes fadeIn {
+		from {
+			transform: scale(0.9);
+			opacity: 0;
+		}
+		to {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+</style>
